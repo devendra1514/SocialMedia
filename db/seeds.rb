167ec12a7e2c00f910725f1761f0ea2776de15a9
You <you@example.com>
2024-11-08 13:13:33 +0530

    modifiy seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-AdminUser.find_or_initialize_by(email: ENV['ADMIN_EMAIL'] || Rails.application.credentials.dig(:admin, :email)) do |admin_user|
-	admin_user.password = ENV['ADMIN_PASSWORD'] || Rails.application.credentials.dig(:admin, :password)
-	admin_user.password_confirmation = ENV['ADMIN_PASSWORD'] ||Rails.application.credentials.dig(:admin, :password)
-	admin_user.save
+if Rails.env.production?
+	AdminUser.find_or_initialize_by(Rails.application.credentials.dig(:admin, :email)) do |admin_user|
+		admin_user.password = Rails.application.credentials.dig(:admin, :password)
+		admin_user.password_confirmation = Rails.application.credentials.dig(:admin, :password)
+		admin_user.save
+	end
+else
+	AdminUser.find_or_initialize_by(email: ENV['ADMIN_EMAIL']) do |admin_user|
+		admin_user.password = ENV['ADMIN_PASSWORD']
+		admin_user.password_confirmation = ENV['ADMIN_PASSWORD']
+		admin_user.save
+	end
 end
 
 Role.find_or_initialize_by(role_id: 'user').save
